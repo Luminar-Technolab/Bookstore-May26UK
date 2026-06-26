f43ee393ed93b1c3e46250e1e580ea2325aef913
soniagkalliath <soniageorge111@gmail.com>
2026-06-26 16:13:11 +0530

    login details updated

--- src/users/components/Header.jsx
+++ src/users/components/Header.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { FaFacebookSquare, FaInstagram, FaUser } from 'react-icons/fa'
 import { FaXTwitter } from 'react-icons/fa6'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 function Header() {
+  const [token,setToken] = useState("")
+  const [dp,setDp] = useState("")
+  const [dropdown,setDropdown] = useState(false)
+  const navigate = useNavigate()
+
+  useEffect(()=>{
+    if(sessionStorage.getItem("token") && sessionStorage.getItem("user")){
+      const userToken = sessionStorage.getItem("token")
+      const user = JSON.parse(sessionStorage.getItem("user"))
+      setToken(userToken)
+      setDp(user.picture)
+    }
+  },[token])
+
+  const logout = ()=>{
+    sessionStorage.clear()
+    setToken("")
+    setDp("")
+    setDropdown(false)
+    navigate('/')
+  }
+
   return (
     <>
     {/* upper part */}
@@ -24,7 +46,27 @@ function Header() {
         <FaXTwitter className='mx-1'/>
         <FaFacebookSquare/>
         {/* login link */}
-        <Link to={'/login'} className='flex items-center border rounded py-2 px-3 ms-3 hover:bg-black hover:text-white'> <FaUser className='me-1'/> Login </Link>
+        {
+          !token ?
+          <Link to={'/login'} className='flex items-center border rounded py-2 px-3 ms-3 hover:bg-black hover:text-white'> <FaUser className='me-1'/> Login </Link>
+          :
+          <div>
+            {/* profile icon */}
+            <button onClick={()=>setDropdown(!dropdown)} className="shadow-sm rounded ms-5 p-1">
+              <img width={'40px'} height={'40px'} style={{borderRadius:'50%'}} src={dp==""?"https://www.pngkey.com/png/full/349-3499617_person-placeholder-person-placeholder.png":dp} alt="profile icon" />
+            </button>
+            {/* drop down menu */}
+            {
+              dropdown &&
+              <div className="absolute right-0 z-10 mt-2 w-40 bg-white shadow rounded ring-1 ring-black/5 p-2">
+                {/* profile link */}
+                <Link to={'/profile'} className='flex px-3 py-2'>Profile</Link>
+                {/* logout btn */}
+                <button onClick={logout} className='flex cursor-pointer px-3 py-2'>Logout</button>
+              </div>
+            }
+          </div>
+        }
       </div>
     </div>
     {/* lower part */}
